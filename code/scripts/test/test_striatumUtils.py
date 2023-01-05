e1301dcf8b385feb437c14c399bfa9e808e665b4
Joaquin Rapela <joacorapela@gmail.com>
2023-01-05 16:45:37 +0000

    Added test_findTrialsIDsOfPerfectSequences

--- code/scripts/test/test_striatumUtils.py
+++ code/scripts/test/test_striatumUtils.py
@@ -21,8 +21,25 @@ def test_findCorrectSequencesStartAndEndIndices():
     assert(correct_sequences_start_and_end_indices[1] == (16,20))
 
 
+def test_findTrialsIDsOfPerfectSequences():
+    perfect_sequence = "21637"
+    # data = [3,2,2,1,6,6,3,3,3,7,4]
+    data_start_ports = [3,2,1,6,3,7,4,2,1,6,3,2,1,6,3,7,2]
+    data_trials_ids  = [0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4]
+    correct_trials_ids = [1, 3]
+    transitions_data = pd.DataFrame({"Start_Port": data_start_ports,
+                                     "Trial_id": data_trials_ids})
+    trials_ids = striatumUtils.findTrialsIDsOfPerfectSequences(
+        perfect_sequence=perfect_sequence, transitions_data=transitions_data)
+    print(trials_ids)
+    assert(len(correct_trials_ids) == len(trials_ids))
+    for i in range(len(correct_trials_ids)):
+        assert(correct_trials_ids[i] == trials_ids[i])
+
+
 def main(argv):
-    test_findCorrectSequencesStartAndEndIndices()
+    # test_findCorrectSequencesStartAndEndIndices()
+    test_findTrialsIDsOfPerfectSequences()
 
 
 if __name__ == "__main__":
